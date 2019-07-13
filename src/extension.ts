446ceed5ea6a7a15c65cc06a2aeb905e87c650e1
Georgi Parlakov <gparlakov@gmail.com>
2019-07-13 19:27:31 +0300

    Fixed the breaking webpack build 1. Added webpack types to package json 2. Excluded test.me.ts file from build (in tsconfig) 3. Do not import the schematics.js file from angular devkit - it breaks webpack for some reason

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,9 @@ import { main as schematics } from "@angular-devkit/schematics-cli/bin/schematic
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "scuri-code" is now active!');
+  console.log("test");
+  const channel = vscode.window.createOutputChannel("SCURI");
+  channel.appendLine('Congratulations, your extension "scuri-code" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
@@ -18,8 +20,16 @@ export function activate(context: vscode.ExtensionContext) {
 
     const a = vscode.window.activeTextEditor;
     if (a !== undefined) {
-      // Display a message box to the user
-      runScuriSchematic(a.document.fileName);
+      const root = vscode.workspace.getWorkspaceFolder(a.document.uri);
+      if (root === undefined) {
+        vscode.window.showErrorMessage(
+          "There needs to be an open folder with an angular app inside it for SCURI to generate/update specs."
+        );
+      } else {
+        runScuriSchematic(a.document.fileName, root.uri.fsPath, channel);
+      }
+    } else {
+      vscode.window.showErrorMessage("A file needs to be opened for SCURI to generate/update specs for.");
     }
   });
 
@@ -29,17 +39,19 @@ export function activate(context: vscode.ExtensionContext) {
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
-function runScuriSchematic(activeFileName: string) {
-  schematics({
-    args: [
-      "scuri:spec",
-      "--name",
-      activeFileName,
-      "--workingDirectory",
-      "c:\\Users\\gparl\\projects\\scuri-code"
-    ]
-  })
-    .then()
-    .catch()
-    .finally();
+function runScuriSchematic(activeFileName: string, root: string, channel: vscode.OutputChannel) {
+  channel.appendLine(`Working on ${activeFileName} in root: ${root}`);
+  channel.show(true);
+  // schematics({
+  //   args: [
+  //     "scuri:spec",
+  //     "--name",
+  //     activeFileName,
+  //     "--workingDirectory",
+  //     "c:\\Users\\gparl\\projects\\scuri-code"
+  //   ]
+  // })
+  //   .then()
+  //   .catch()
+  //   .finally();
 }
